3691a73bb1bf7e082a81d0549c04a5bc4161aa05
baekyoung1 <111644944+baekyoung1@users.noreply.github.com>
2022-09-21 16:53:50 +0900

    Update App.js

--- rn-chat/src/App.js
+++ rn-chat/src/App.js
@@ -1,22 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StatusBar } from 'react-native';
-import { ThemeProvider } from 'styled-components/native';
-import { theme } from './theme';
+import styled,{ ThemeProvider } from 'styled-components/native';
+import { theme, dark } from './theme';
 import Navigation from './navigations';
 import { UserProvider, ProgressProvider } from './contexts';
+import {View, StyleSheet, Switch} from 'react-native';
+import { Profile } from './screens';
 
+const Container = styled.SafeAreaView`
+  background-color: ${({ theme }) => theme.background};
+  align-items: flex-end;
+  justify-content: flex-start;
+  padding: 5px;
+
+`;
+/*onst styles = StyleSheet.create({
+  container: {
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    backgroundColor: "#d4e6ff",
+    padding: 10,
+  },
+  
+});*/
 const App = () => {
+
+
+  const [isDark, setIsDark] = useState(false);
+  const _toggleSwitch = () => setIsDark(!isDark);
   return (
-    <ThemeProvider theme={theme}>
-      <ProgressProvider>
-        <UserProvider>
-          <StatusBar
-            backgroundColor={theme.background}
-            barStyle="dark-content"
-          />
-          <Navigation />
-        </UserProvider>
-      </ProgressProvider>
+    <ThemeProvider theme={isDark ? dark : theme}>
+      
+        <ProgressProvider>
+          <UserProvider>
+            <StatusBar
+              backgroundColor={theme.background}
+              barStyle="dark-content"
+            />
+              <Container>
+                  <Switch
+                    trackColor={{false: '#767577', true: '#81b0ff'}}
+                    thumbColor= '#ffffff'
+                    ios_backgroundColor={theme.background}
+                    onValueChange={_toggleSwitch}
+                    backgroundColor={theme.background}
+                    value={isDark}
+                    
+                  />
+              </Container>
+            <Navigation />
+          </UserProvider>
+        </ProgressProvider>
     </ThemeProvider>
   );
 };
